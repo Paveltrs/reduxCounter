c8a365460e708f51a7a7a9a74a6ff1c2c9c3d9ea
Pavel <84628503+Paveltrs@users.noreply.github.com>
2021-09-24 20:41:40 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import thunk from 'redux-thunk'
+import './styles.css'
+import {applyMiddleware, createStore} from 'redux'
+import { rootReducer } from './redux/rootReducer'
+import {decrement, increment, asyncIncrement, changeTheme} from './redux/actions'
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+
+addBtn.addEventListener('click', () => {
+    store.dispatch(increment())
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement())
+})
+
+asyncBtn.addEventListener('click', () => {
+  store.dispatch(asyncIncrement())
+})
+
+themeBtn.addEventListener('click', () => {
+     const newTheme = document.body.classList.contains('light')
+    ? 'dark'
+    : 'light'
+    store.dispatch(changeTheme(newTheme))
+})
+
+
+store.subscribe(() => {
+    const state = store.getState()
+
+    counter.textContent = state.counter
+    document.body.className = state.theme.value;
+
+    [addBtn, subBtn, themeBtn, asyncBtn].forEach(btn => {
+        btn.disabled = state.theme.disabled
+    })
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
